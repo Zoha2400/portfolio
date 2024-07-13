f611223600359e20b3b9fbf6436b4812704e623f
Zohir <vasv29834@gmail.com>
2024-07-14 00:19:31 +0500

    Statues

--- src/components/Statues.tsx
+++ src/components/Statues.tsx
@@ -2,15 +2,33 @@ import Image from "next/image";
 function Statues() {
   return (
     <div className="flex justify-center items-center">
-      <div className="relative w-full flex justify-between  mt-40 bg-black ">
-        <Image
-          src="/simone.png"
-          alt="Phone of Console"
-          className={`transition w-auto h-96 scale-x-[-1]`}
-          objectFit="contain"
-          width={500}
-          height={300}
-        />
+      <div className="w-full flex justify-between relative mt-40 mb-40 p-2 bg-black ">
+        <div className="imgs flex justify-center">
+          <Image
+            src="/simone.png"
+            alt="Phone of Console"
+            className={`transition w-auto h-96 scale-x-[-1] z-20`}
+            objectFit="contain"
+            width={500}
+            height={300}
+          />
+          <Image
+            src="/simone.png"
+            alt="Phone of Console"
+            className={`transition w-auto h-96 scale-x-[-1] absolute z-10 ml-48 opacity-75`}
+            objectFit="contain"
+            width={500}
+            height={300}
+          />
+          <Image
+            src="/simone.png"
+            alt="Phone of Console"
+            className={`transition w-auto h-96 scale-x-[-1] absolute z-0 ml-96 opacity-50`}
+            objectFit="contain"
+            width={500}
+            height={300}
+          />
+        </div>
         <div className="text h-96 relative flex flex-col justify-between">
           <p className="text-6xl text-gray-900 absolute z-0 m-40">
             Driven by the absurd, crafting digital wonders.
