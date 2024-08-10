4c443931225aca5fbe75bbc714df6abb24a1be45
Zoha2400 <vasv29834@gmail.com>
2024-08-10 12:10:13 +0500

    Stack page and some fixed code

---
+++ src/app/stack/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="text-white p-8 font-mono">
+      <p>
+        If you wanted to order a website, but couldn't find the right person for
+        this job - Congratulations, you found me!)
+      </p>
+    </div>
+  );
+}
+
+export default page;
