02764bc03f63fb5fb3dd12d462c589e910ea07fa
Zoha2400 <vasv29834@gmail.com>
2024-08-17 02:49:44 +0500

    feat: Links props

--- src/components/ui/StackEl.tsx
+++ src/components/ui/StackEl.tsx
@@ -7,30 +7,34 @@ interface StackElProps {
   icon_name: string;
   name: string;
   color?: string;
+  link?: string;
 }
 
 const StackEl: React.FC<StackElProps> = ({
   icon_name,
   name,
   color = "#52525b",
+  link = "https://www.google.com/",
 }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div
-      className={`p-2 flex justify-center items-center gap-1 rounded-md transition-all duration-300 cursor-pointer ${
-        isHovered ? `shadow-lg` : ""
-      }`}
-      style={{
-        backgroundColor: color,
-        boxShadow: isHovered ? `0px 0px 20px 3px ${color}` : "none", // Тень появляется только при наведении
-      }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <Icon icon={icon_name} />
-      <p>{name}</p>
-    </div>
+    <a href={`${link}`}>
+      <div
+        className={`p-2 flex justify-center items-center gap-1 rounded-md transition-all duration-300 cursor-pointer ${
+          isHovered ? `shadow-lg` : ""
+        }`}
+        style={{
+          backgroundColor: color,
+          boxShadow: isHovered ? `0px 0px 20px 3px ${color}` : "none", // Тень появляется только при наведении
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <Icon icon={icon_name} />
+        <p>{name}</p>
+      </div>
+    </a>
   );
 };
 
