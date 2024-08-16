019073562273520d7ec81e57d3a5f6a2d74a5578
Zoha2400 <vasv29834@gmail.com>
2024-08-17 02:47:22 +0500

    feat: all my stack

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -8,14 +8,100 @@ function Page() {
         this job - Congratulations, you found me!
       </p>
 
-      <div className="stack flex gap-2 text-sm">
+      <div className="stack flex gap-2 flex-wrap text-sm">
         <StackEl icon_name="devicon:react" name="React" color="#4b60ce" />
+        <StackEl icon_name="devicon:nextjs" name="Next.js" color="#252525" />
+        <StackEl icon_name="devicon:nestjs" name="Nest.js" color="#7f1340" />
+        <StackEl icon_name="devicon:flask" name="Flask" color="#b8b8b8" />
+        <StackEl icon_name="devicon:python" name="Python" color="#004781" />
+        <StackEl
+          icon_name="devicon:javascript"
+          name="JavaScript"
+          color="#bbb200"
+        />
+        <StackEl
+          icon_name="devicon:postgresql"
+          name="PostgreSQL"
+          color="#4c00bf"
+        />
+        <StackEl icon_name="devicon:css3" name="CSS" color="#004781" />
+        <StackEl icon_name="devicon:html5" name="HTML" color="#004781" />
+
+        <StackEl icon_name="devicon:sass" name="SASS" color="#004781" />
+        <StackEl
+          icon_name="devicon:tailwindcss"
+          name="Tailwind"
+          color="#004781"
+        />
+
+        <StackEl icon_name="devicon:github" name="GitHub" color="#004781" />
+        <StackEl icon_name="devicon:git" name="Git" color="#004781" />
+        <StackEl icon_name="devicon:jira" name="Jira" color="#004781" />
+        <StackEl icon_name="devicon:docker" name="Docker" color="#004781" />
+        <StackEl
+          icon_name="devicon:kubernetes"
+          name="Kubernetes"
+          color="#004781"
+        />
+
         <StackEl icon_name="devicon:nodejs" name="Node.js" color="#1a6335" />
+
         <StackEl
           icon_name="devicon:typescript"
           name="TypeScript"
           color="#022954"
         />
+        <StackEl icon_name="devicon:vite" name="Vite" color="#004781" />
+        <StackEl icon_name="devicon:webpack" name="Webpack" color="#004781" />
+        <StackEl icon_name="devicon:rust" name="Rust" color="#004781" />
+        <StackEl icon_name="devicon:vuejs" name="Vue3" color="#004781" />
+        <StackEl icon_name="devicon:svelte" name="Svelte" color="#004781" />
+        <StackEl icon_name="devicon:mobx" name="Mobx" color="#004781" />
+        <StackEl icon_name="devicon:redux" name="Redux" color="#004781" />
+        <StackEl
+          icon_name="devicon:sequelize"
+          name="Sequelize"
+          color="#004781"
+        />
+        <StackEl
+          icon_name="devicon:materializecss"
+          name="Materialize"
+          color="#004781"
+        />
+        <StackEl
+          icon_name="devicon:bootstrap"
+          name="Bootstrap"
+          color="#004781"
+        />
+        <StackEl icon_name="devicon:intellij" name="IntelliJ" color="#004781" />
+        <StackEl icon_name="devicon:insomnia" name="Insomnia" color="#004781" />
+
+        <StackEl icon_name="devicon:postman" name="Postman" color="#004781" />
+        <StackEl icon_name="devicon:json" name="JSON" color="#004781" />
+        <StackEl icon_name="devicon:express" name="Express" color="#004781" />
+        <StackEl icon_name="devicon:npm" name="Npm" color="#004781" />
+        <StackEl icon_name="devicon:yarn" name="Yarn" color="#004781" />
+        <StackEl icon_name="devicon:redis" name="Redis" color="#004781" />
+        <StackEl icon_name="devicon:rabbitmq" name="RabbitMQ" color="#004781" />
+        <StackEl icon_name="devicon:linux" name="Linux" color="#004781" />
+        <StackEl icon_name="devicon:ubuntu" name="Ubuntu" color="#004781" />
+        <StackEl icon_name="devicon:babel" name="Babel" color="#004781" />
+        <StackEl
+          icon_name="devicon:materialui"
+          name="MaterialUi"
+          color="#004781"
+        />
+        <StackEl icon_name="devicon:nginx" name="Nginx" color="#004781" />
+        <StackEl icon_name="devicon:cplusplus" name="C++" color="#004781" />
+        <StackEl icon_name="devicon:bun" name="BunJs" color="#004781" />
+        <StackEl icon_name="devicon:figma" name="Figma" color="#004781" />
+        <StackEl
+          icon_name="devicon:photoshop"
+          name="Photoshop"
+          color="#004781"
+        />
+        <StackEl icon_name="devicon:grpc" name="gRPC" color="#004781" />
+        <StackEl icon_name="devicon:ngrok" name="Ngrok" color="#004781" />
       </div>
     </div>
   );
